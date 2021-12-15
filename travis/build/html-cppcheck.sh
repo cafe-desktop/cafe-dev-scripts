116f69171eb0162b3503312a6cc7fbc44980fcaa
Pablo Barciela <scow@riseup.net>
2021-12-15 17:55:02 +0100

    add ARGS_CPPCHECK variable

--- travis/build/html-cppcheck.sh
+++ travis/build/html-cppcheck.sh
@@ -4,5 +4,5 @@ set -v
 set -x
 
 export TITLECPPCHECK="${REPO_NAME} (cppcheck `dpkg -s cppcheck|grep -i version|cut -d " " -f 2`)"
-cppcheck --force --xml --output-file=cppcheck.xml --enable=warning,style,performance,portability,information,missingInclude .
+cppcheck --xml --output-file=cppcheck.xml $ARGS_CPPCHECK .
 cppcheck-htmlreport --title="$TITLECPPCHECK" --file=cppcheck.xml --report-dir=cppcheck-htmlreport
