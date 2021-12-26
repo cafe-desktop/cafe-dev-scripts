ce983c1bd9c0906c6b52f39900f704c2f42e7912
Pablo Barciela <scow@riseup.net>
2021-12-26 17:44:01 +0100

    build: install-dockerbuild: add little more changes

--- travis/build/install-dockerbuild.sh
+++ travis/build/install-dockerbuild.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+set -v
+set -x
+
 curl -Ls -o docker-build https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/docker-build
 curl -Ls -o gen-index https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/gen-index.sh
 curl -Ls -o html-cppcheck https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/html-cppcheck.sh
