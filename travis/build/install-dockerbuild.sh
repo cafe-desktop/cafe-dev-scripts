841af33cc497792febfc79dc6930b97af191687f
Pablo Barciela <scow@riseup.net>
2021-12-28 18:09:14 +0100

    build: add 'install-from-git' script

--- travis/build/install-dockerbuild.sh
+++ travis/build/install-dockerbuild.sh
@@ -6,4 +6,5 @@ set -x
 curl -Ls -o docker-build https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/docker-build
 curl -Ls -o gen-index https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/gen-index.sh
 curl -Ls -o html-cppcheck https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/html-cppcheck.sh
-chmod +x docker-build gen-index html-cppcheck
+curl -Ls -o install-from-git https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/install-from-git.sh
+chmod +x docker-build gen-index html-cppcheck install-from-git
---
+++ travis/build/install-from-git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -v
+set -x
+
+# Install git repository from branch
+cd ${START_DIR}
+git clone --depth 1 $1.git -b $2 tmp-install
+cd tmp-install
+./autogen.sh
+if [ ${DISTRO_NAME} == "debian" -o ${DISTRO_NAME} == "ubuntu" ];then
+    ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu --libexecdir=/usr/lib/x86_64-linux-gnu
+else
+    ./configure --prefix=/usr
+fi
+make
+make install
+cd ..
+rm -rf tmp-install
