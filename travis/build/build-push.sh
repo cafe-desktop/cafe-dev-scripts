9907836eca3d5498713b9b60b1f96b6628c4a8a4
Pablo Barciela <scow@riseup.net>
2021-12-29 00:05:29 +0100

    build: build-push.sh: distro as argument, if not, debian testing is default

--- travis/build/build-push.sh
+++ travis/build/build-push.sh
@@ -3,7 +3,14 @@
 set -v
 set -x
 
+if [ $1 ]
+then
+  export DISTRO=$1
+else
+  export DISTRO='debian'
+fi
+
 export TRAVIS_COMMIT=$GITHUB_SHA
 export TRAVIS_BRANCH=$(echo $GITHUB_REF |cut -d "/" -f3)
-./docker-build --name "debian" --config .build.yml --install
-./docker-build --name "debian" --verbose --config .build.yml --build scripts
+./docker-build --name "$DISTRO" --config .build.yml --install
+./docker-build --name "$DISTRO" --verbose --config .build.yml --build scripts
