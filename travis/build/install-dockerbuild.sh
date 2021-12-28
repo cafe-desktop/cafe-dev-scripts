adf5c47bd645382cfcd3866e2d8acd64c5f7dbc1
Pablo Barciela <scow@riseup.net>
2021-12-28 19:14:26 +0100

    build: add 'install-from-debs' script

--- travis/build/install-dockerbuild.sh
+++ travis/build/install-dockerbuild.sh
@@ -7,4 +7,5 @@ curl -Ls -o docker-build https://github.com/cafe-desktop/cafe-dev-scripts/raw/ma
 curl -Ls -o gen-index https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/gen-index.sh
 curl -Ls -o html-cppcheck https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/html-cppcheck.sh
 curl -Ls -o install-from-git https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/install-from-git.sh
-chmod +x docker-build gen-index html-cppcheck install-from-git
+curl -Ls -o install-from-debs https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/install-from-debs.sh
+chmod +x docker-build gen-index html-cppcheck install-from-git install-from-debs
---
+++ travis/build/install-from-debs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -v
+set -x
+
+# Install .deb packages from release branch
+git clone $1.git -b $2 tmp-install
+cd tmp-install
+for var in "${@:3}"
+do
+  wget $1/releases/download/`git describe --abbrev=0 --tags`/"$var"_`git describe --abbrev=0 --tags|sed 's/^.//'`-1_amd64.deb
+done
+dpkg -i *.deb
+cd ..
+rm -rf tmp-install
