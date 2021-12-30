019da721c01dd1d93078e67c445840957eb265a9
Pablo Barciela <scow@riseup.net>
2021-12-30 01:27:05 +0100

    build: add 'make-scanbuild' script

--- travis/build/install-dockerbuild.sh
+++ travis/build/install-dockerbuild.sh
@@ -8,4 +8,5 @@ curl -Ls -o gen-index https://github.com/cafe-desktop/cafe-dev-scripts/raw/maste
 curl -Ls -o html-cppcheck https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/html-cppcheck.sh
 curl -Ls -o install-from-git https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/install-from-git.sh
 curl -Ls -o install-from-debs https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/install-from-debs.sh
-chmod +x docker-build gen-index html-cppcheck install-from-git install-from-debs
+curl -Ls -o make-scanbuild https://github.com/cafe-desktop/cafe-dev-scripts/raw/master/travis/build/make-scanbuild.sh
+chmod +x docker-build gen-index html-cppcheck install-from-git install-from-debs make-scanbuild
---
+++ travis/build/make-scanbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -v
+set -x
+
+export TITLESCANBUILD="${REPO_NAME} (clang-tools `dpkg -s clang-tools|grep -i version|cut -d " " -f 2`) - scan-build results"
+NOCONFIGURE=1 ./autogen.sh
+scan-build $CHECKERS ./configure $1
+if [ $CPU_COUNT -gt 1 ]; then
+    unbuffer scan-build $CHECKERS --html-title="$TITLESCANBUILD" --keep-cc -o html-report make -j $(( $CPU_COUNT + 1 )) 2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
+else
+    unbuffer scan-build $CHECKERS --html-title="$TITLESCANBUILD" --keep-cc -o html-report make 2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
+fi
