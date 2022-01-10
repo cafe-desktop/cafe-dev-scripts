ffb370e6304e1ebe3f477790014244e01402e1e3
Pablo Barciela <scow@riseup.net>
2022-01-10 20:13:01 +0100

    build: after-build: show cafe-common libctk-3-dev package versions

--- travis/build/after-build.sh
+++ travis/build/after-build.sh
@@ -3,6 +3,7 @@
 set -v
 set -x
 
+unbuffer apt list --installed cafe-common libctk-3-dev 2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
 cat ./html-report/output_${TRAVIS_COMMIT} | sed 's,\x1B\[[0-9;]*[a-zA-Z],,g' > tmpfile #remove colors in the logs
 cat tmpfile > ./html-report/output_${TRAVIS_COMMIT}
 echo Errors detected in the build `cat ./html-report/output_${TRAVIS_COMMIT} | grep ' error:' | grep -v "\[" | wc -l` 2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
