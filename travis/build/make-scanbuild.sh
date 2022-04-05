d1b605c27b35a1197257ae124d3343e48c964ca2
Pablo Barciela <scow@riseup.net>
2022-04-05 22:03:11 +0200

    build: make-scanbuild: save autogen and configure output in the logs

--- travis/build/make-scanbuild.sh
+++ travis/build/make-scanbuild.sh
@@ -5,8 +5,14 @@ set -v
 set -x
 
 export TITLESCANBUILD="${REPO_NAME} (clang-tools `dpkg -s clang-tools|grep -i version|cut -d " " -f 2`) - scan-build results"
-NOCONFIGURE=1 ./autogen.sh
-scan-build $CHECKERS ./configure $1
+NOCONFIGURE=1 unbuffer ./autogen.sh  2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
+if [ ${PIPESTATUS[0]} -ne 0 ];then
+    exit 1
+fi
+unbuffer scan-build $CHECKERS ./configure $1  2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
+if [ ${PIPESTATUS[0]} -ne 0 ];then
+    exit 1
+fi
 
 if [ $CPU_COUNT -gt 1 ]; then
     unbuffer scan-build $CHECKERS --html-title="$TITLESCANBUILD" --keep-cc -o html-report make -j $(( $CPU_COUNT + 1 )) 2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
