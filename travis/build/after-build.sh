bc24377b8e18169f71a35c49960245d81efc4add
Pablo Barciela <scow@riseup.net>
2022-04-08 14:58:36 +0200

    after-build: show cppcheck defects in the logs

--- travis/build/after-build.sh
+++ travis/build/after-build.sh
@@ -32,7 +32,10 @@ echo Warnings detected in the logs `cat ./html-report/output_${TRAVIS_COMMIT} | 
 if [ ${PIPESTATUS[0]} -ne 0 ];then
     exit 1
 fi
-
+echo cppcheck defects detected in the logs `cat ./cppcheck-htmlreport/index.html |grep total|sed 's/           <tr><td><\/td><td>//g'|sed 's/<\/td><td>total<\/td><\/tr>//g'` 2>&1 | tee -a ./html-report/output_${TRAVIS_COMMIT}
+if [ ${PIPESTATUS[0]} -ne 0 ];then
+    exit 1
+fi
 if grep -w "`basename "${0}" .sh`: line" ./html-report/output_${TRAVIS_COMMIT}; then
   exit 1
 fi
