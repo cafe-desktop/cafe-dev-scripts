d3c00ff68575ffa3232a7026d679971049718115
Pablo Barciela <scow@riseup.net>
2022-08-30 19:14:06 +0200

    translation-sync: add update-help script

---
+++ translation-sync/update-help.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -v
+set -x
+
+for dir in ./*/     # list directories in the form "/tmp/dirname/"
+do
+    dir=${dir%*/}      # remove the trailing "/"
+#    echo "${dir##*/}"    # print everything after the final "/"
+#    echo "./${dir##*/}/*.po"    # print everything after the final "/"
+    msgmerge -U ./${dir##*/}/*.po $1;
+done
