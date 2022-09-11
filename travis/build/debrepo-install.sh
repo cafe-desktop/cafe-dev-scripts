a308046eabef1694a965344116b00e1e616d1def
Pablo Barciela <scow@riseup.net>
2022-09-11 15:11:48 +0200

    debrepo-install: show error with 'unmet dependencies'

--- travis/build/debrepo-install.sh
+++ travis/build/debrepo-install.sh
@@ -7,4 +7,7 @@ set -x
 # Install .deb packages from release branch
 echo deb [trusted=yes] https://cafe-desktop.github.io/debian-packages/ ./ >> /etc/apt/sources.list
 aptitude -y update
-aptitude -y install $1
+aptitude -y install $1 > aptlog
+if grep -w "unmet dependencies" aptlog; then
+  exit 1
+fi
