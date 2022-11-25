84178b2aa995f7ecbbaeadfa05b6be76db9bc1fd
Pablo Barciela <scow@riseup.net>
2022-11-25 11:26:32 +0100

    utils: add trailing_spaces.sh script

---
+++ travis/build/utils/trailing_spaces.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [[ -d ".git" ]]
+then
+    egrep --exclude=\*.{png,svg,xcf} --exclude-dir=.git -lRZ $1 . | xargs -0 -l sed -i -e 's/[[:space:]]\+$//'
+    if [ ${PIPESTATUS[0]} -ne 0 ];then
+        exit 1
+    fi
+    echo done
+else
+  echo "you aren't inside git repository"
+  exit 1
+fi
