5f656f4b89ca8ae67ec5ec952d03537899c85baa
Pablo Barciela <scow@riseup.net>
2023-04-06 12:54:17 +0200

    utils: add 'remlines-after.sh' script

---
+++ travis/build/utils/remlines-after.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [[ -d ".git" ]]
+then
+  if [ $# -eq 1 ]
+  then
+    egrep --exclude-dir=.git -lRZ $1 . | xargs -0 -l sed -i -e '/'$1'/q'
+    if [ ${PIPESTATUS[0]} -ne 0 ];then
+        exit 1
+    fi
+    echo done
+  else
+    echo "1 argument is required"
+    exit
+  fi
+else
+  echo "you aren't inside git repository"
+  exit 1
+fi
