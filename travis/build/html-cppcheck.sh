73ac64d500398c718020da0f59675a6bb19bcce7
Pablo Barciela <scow@riseup.net>
2023-10-25 12:42:46 +0200

    html-cppcheck: dpkg -l instead -s to show cppcheck version

--- travis/build/html-cppcheck.sh
+++ travis/build/html-cppcheck.sh
@@ -4,6 +4,6 @@ set -e
 set -v
 set -x
 
-export TITLECPPCHECK="${REPO_NAME} (cppcheck `dpkg -s cppcheck|grep -i version|cut -d " " -f 2`)"
+export TITLECPPCHECK="${REPO_NAME} (cppcheck `dpkg -l cppcheck|grep cppcheck|cut -d " " -f10`)"
 cppcheck --xml --output-file=cppcheck.xml $ARGS_CPPCHECK .
 cppcheck-htmlreport --title="$TITLECPPCHECK" --file=cppcheck.xml --report-dir=cppcheck-htmlreport
