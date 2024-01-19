37a7704948a7ffe0754eb1aa29a9d22042cee3c8
Pablo Barciela <scow@riseup.net>
2024-01-19 11:37:48 +0100

    make-scanbuild: build with --prefix=/usr

--- travis/build/make-scanbuild.sh
+++ travis/build/make-scanbuild.sh
@@ -29,7 +29,7 @@ if [ "${1}" = "meson" ]; then
         exit 1
     fi
 else
-    unbuffer scan-build $CHECKERS ./configure $1  2>&1 | tee -a --output-error=exit ./html-report/output_${TRAVIS_COMMIT}
+    unbuffer scan-build $CHECKERS ./configure --prefix=/usr $1  2>&1 | tee -a --output-error=exit ./html-report/output_${TRAVIS_COMMIT}
     if [ ${PIPESTATUS[0]} -ne 0 ];then
         exit 1
     fi
@@ -46,7 +46,7 @@ else
         fi
     fi
 
-    make install
+    make prefix=/usr install
 
     if [ "${REPO_NAME}" == "baul" ]; then
       xvfb-run make check 2>&1 | tee -a --output-error=exit ./html-report/output_${TRAVIS_COMMIT}
