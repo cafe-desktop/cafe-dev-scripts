038d14595db7c608ee90fd53eb467c78af3a5d88
Pablo Barciela <scow@riseup.net>
2024-07-14 22:54:17 +0200

    exit-warning: ignore warning about libxml

--- travis/build/exit-warning.sh
+++ travis/build/exit-warning.sh
@@ -4,4 +4,4 @@ set -e
 set -v
 set -x
 
-if cat ./html-report/output_${TRAVIS_COMMIT}|grep -i warning|grep -v "autoreconf: export WARNINGS"; then exit 1; fi
+if cat ./html-report/output_${TRAVIS_COMMIT}|grep -i warning|grep -v "autoreconf: export WARNINGS"|grep -v "Warning: program compiled against libxml"; then exit 1; fi
