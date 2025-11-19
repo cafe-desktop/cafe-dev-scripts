b83532fe884793161a751bf3328611d71916ca9c
Pablo Barciela <scow@riseup.net>
2025-11-19 12:18:00 +0100

    build-debs: install expect required for unbuffer
    and check if unbuffer and lintian are installed

--- travis/build/build-debs.sh
+++ travis/build/build-debs.sh
@@ -19,7 +19,8 @@ else
   gitrepo=$3
 fi
 
-aptitude install -y devscripts dh-make dh-exec gdebi lintian
+#expect required for unbuffer
+aptitude install -y devscripts dh-make dh-exec gdebi lintian expect
 cd ${START_DIR}
 mkdir -p html-report
 git clone --depth 1 ${giturl}.git -b ${gitbranch} tmp-debs
@@ -37,6 +38,7 @@ else
   aptitude -f -y install
   dpkg -i *.deb
 fi
+unbuffer lintian &> /dev/null #check if unbuffer and lintian are installed
 unbuffer lintian -i -EIL+pedantic *.changes > lintianlog || echo lintian error!
 cat lintianlog | grep -E '^(E:|W:|I:|X:|P:)' || echo lintian is OK!
 mv *deb *buildinfo *changes debian.tar.xz deb_packages.tar.xz .${START_DIR}/html-report
